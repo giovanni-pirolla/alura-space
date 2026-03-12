d8ea6f063342592d574cd674b416519503e4cade
Giovanni Pirolla <giovannippirolla@gmail.com>
2026-03-12 18:18:30 -0300

    eliminando um arquivo de testes

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-print(f"Executável: {sys.executable}")
-print(f"Caminhos de busca: {sys.path}")
-try:
-    import dotenv
-    print("Dotenv encontrado em:", dotenv.__file__)
-except ImportError:
-    print("Erro: Módulo não encontrado no sys.path")
